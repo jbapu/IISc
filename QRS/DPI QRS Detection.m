af7059e2131caa2e3ded5c30b0f5cd5e3dbadef9
bjayanth <bapujayanth@yahoo.com>
2014-07-11 11:47:23 +0530

    Update DPI QRS Detection.m

--- QRS/DPI QRS Detection.m
+++ QRS/DPI QRS Detection.m
@@ -1,15 +1,17 @@
+%%%%%  QRS Detection Using Dynamic Plosion Index  %%%%%
 clc
 clear
 close all
-y=csvread('D:\ECG\ECG data\custom_psuedo2\ecg_plot_2.csv');
-% y=y(11500:end);
-fs=400;
-time=12;
-tint=time*fs;
+
+y=csvread('D:\ECG\ECG data\mit_bih_100.csv');   %Input data file
+fs=400; %Sampling Rate
+time=4; %Time interval
+tint=time*fs;   %No. of sample in time interval
 run=1;
-% while(run+tint<5000)
+
 x=y(run:run+tint);
 
+%Removing DC value
 avg=0;
 for i=1:length(x)
     avg=avg+x(i);
@@ -17,9 +19,13 @@ end
 avg=avg/length(x);
 x=x-avg;
 
-subplot 311;
+subplot 311;    %Plot Input ECG signal
 plot(x);
 xlim([0 tint]);
+title('Input ECG signal');
+xlabel('Samples -->');
+ylabel('Amplitude -->');
+
 z=tf('z',400);
 
 %HPF
@@ -63,6 +69,9 @@ y9=wrev(y9);
 subplot 312;
 plot(y9);
 xlim([0 tint]);
+title('Filtered ECG signal');
+xlabel('Samples -->');
+ylabel('Amplitude -->');
 x=y9;
 
 % max1=max(x);
@@ -70,34 +79,35 @@ x=y9;
 % x=x(1:end-201);
 % subplot 312;
 % plot(x);
-[pks,locs]=findpeaks(x,'minpeakdistance',200);
-locs
+
 ind = find(x);
 savg=0;
-% n0=ind(1);
-n0=1;
+
+% n0(1)=1;
 diffloc=zeros(1,5);
-%pi=zeros(1,tint);
 
-in=1;
 
-while(in<=ceil(time*225/60) && n0<tint-110)
+ind1=1;
+ind2=1;
+n0 = ones(1,ceil(time*225/60));
+n0(1)=ind(1);
+while(ind1<=ceil(time*225/60) && n0(ind2)<tint-110)
 m=0;
 pi=zeros(1,end);
-if n0<5
-    m1=-n0;
+if n0(ind2)<5
+    m1=-n0(ind2);
 else
     m1=-5;
 end
-for m2=1:tint-(n0+m1)
-    for i=n0+m1+1:n0+m1+m2
+for m2=1:tint-(n0(ind2)+m1)
+    for i=n0(ind2)+m1+1:n0(ind2)+m1+m2
         savg = savg + x(i);
     end
     savg = savg/(m2^(1/2));
     if savg==0
-        pi(m+n0)=0;
+        pi(m+n0(ind2))=0;
     else
-        pi(m+n0) = x(n0)/savg;
+        pi(m+n0(ind2)) = x(n0(ind2))/savg;
     end
     savg=0;
     m=m+1;
@@ -105,13 +115,16 @@ end
 subplot 313;
 plot(pi);
     xlim([0 tint]);
+title('DPI');
+xlabel('Samples -->');
+ylabel('Amplitude -->');
 diff1=0;
-if in==1
+if ind1==1
     chk=10;
 else
     chk=110;
 end
-for i=n0+chk:tint-1
+for i=n0(ind2)+chk:tint-1
     %         if i==n0+chk
     %         else
     if pi(i)>pi(i+1) & pi(i)>pi(i-1)
@@ -132,35 +145,41 @@ for i=n0+chk:tint-1
         if temp>diff1
             diff1=temp;
             %                             fprintf('%f\n',diff1);
-            diffloc(in)=(maxloc+minloc)/2;
+            diffloc(ind1)=(maxloc+minloc)/2;
             %                        fprintf('%f\n',diffloc(in));
         end
         flag=0;
     end
     %         end
 end
-if in==1
+if ind1==1
     temp2=diff1;
 end
 %     fprintf(' prev - %f\n',temp2);
 if diff1>0.3*temp2
-    n0=round(diffloc(in));
-%         fprintf('n0 = %d\n',n0);
-%     if n0>100 && in==1
-%         max1=0;
-%         for k=1:300
-%             if x(k)>max1
-%                 max1=x(k);
-%                 n0=k;
-%             end
-%         end
-%     end
+    ind2=ind2+1;
+    n0(ind2)=round(diffloc(ind1));
+%             fprintf('n0 = %d\n',n0);
+    if n0(ind2)>100 && ind1==1
+        max1=0;
+        for k=1:300
+            if x(k)>max1
+                max1=x(k);
+                n0(ind2)=k;
+            end
+        end
+    end
     temp2=diff1;
-    fprintf('n0 = %d\n',n0);
+    fprintf('Heart beat detected at sample = %d\n',n0(ind2));
 end
 
 pause(0.001);
-in=in+1;
+ind1=ind1+1;
 end
-run=run+tint;
-% end
+n0=n0.*(n0>1);
+ind=find(n0);
+n0=n0(ind);
+hr=60./(diff(n0)/fs);
+avghr = sum(hr)/numel(hr);
+fprintf('Instantaneous Heart Rate = %f\n',hr);
+fprintf('\nAverage Heart Rate  = %f\n',avghr);
